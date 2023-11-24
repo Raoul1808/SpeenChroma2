37b40d715f9a58755cae3c4eb0b0167790d36b53
Raoul1808 <raoulthegeek@gmail.com>
2023-11-24 22:22:59 +0100

    Initial Commit

---
+++ SpeenChroma2/Main.cs
@@ -0,0 +1,17 @@
+﻿using BepInEx;
+
+namespace SpeenChroma2
+{
+    [BepInPlugin(Guid, Name, Version)]
+    public class Main : BaseUnityPlugin
+    {
+        private const string Guid = "srxd.raoul1808.speenchroma2";
+        private const string Name = "Speen Chroma 2";
+        private const string Version = "0.1.0";
+        
+        private void Awake()
+        {
+            Logger.LogMessage("Hi from Speen Chroma 2!");
+        }
+    }
+}
